903f182f61c157a4eed8912439021b40173dc2eb
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-06 21:16:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,6 +14,15 @@ def using_flatten(array)
   array.flatten 
 end
 
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
+
+  
 
   
   
